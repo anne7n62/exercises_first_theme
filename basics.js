c2c25c4c18426b5551a554a81a2232712c314b5c
Annemette <anne7n62@stud.kea.dk>
2021-02-06 11:00:47 +0100

    some concacenation exercises

--- basics.js
+++ basics.js
@@ -1,1 +1,34 @@
-console.log ("JavaScript is running")
+"use strict";
+
+console.log("We are running");
+
+//Different datatypes
+//Write bool, etc., to see the data in the console
+
+const bool = true;
+const num = 27;
+const str = "Annemette";
+const obj = {
+    dogs: 2,
+    bike: 1
+};
+const nothing = null;
+let undf;
+const symbol = symbol("symbol");
+
+//We can also use typeof in the console - will tell you what type it is
+typeof bool;
+typeof num;
+
+// testing concatenation
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+n1 + n2;
+s1 + s2;
+
+n1 + s2;
+s1 + n2;
