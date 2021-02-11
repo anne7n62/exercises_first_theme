fa56f7f8a1c8a384b0e4d85aeb64751b89d1b4eb
Annemette <anne7n62@stud.kea.dk>
2021-02-11 14:55:44 +0100

    pushed

--- capitalization.js
+++ capitalization.js
@@ -5,3 +5,4 @@ const first = firstname.substring(0,1).toUpperCase();
 const second = firstname.substring(1,9).toLowerCase();
 
 console.log (`${first}${second}`);
+
---
+++ loops.js
@@ -0,0 +1,15 @@
+"use strict";
+let counter;
+
+// tælle fra 0 - 10
+// for (counter=0; counter < 10; counter++) {
+//     console.log(counter);
+// }
+
+// console.log(`after the counter is ${counter}`);
+
+for (counter=1; counter < 19; counter += 2) {
+    console.log(counter);
+}
+
+console.log(`after the counter is ${counter}`);
